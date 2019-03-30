83da1560121672781c75cac624d97e8dede14443
mdy6099 <mdy6099@naver.com>
2019-03-31 03:31:49 +0900

    made home page and instruction, linked, add some style

--- README.md
+++
@@ -1,2 +0,0 @@
-# coding-highway-site
-this is real implementation of coding-highway-repository to formal site using html, css, js
---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $(".titleB").on("mouseenter" ,function() {//making photo show
+        $(".describe").hide();
+        $(".highway").show();
+    });
+    $(".defB").on("mouseenter" ,function() {
+        $(".describe").hide();
+        $(".sunclear").show();
+    });
+    $(".playB").on("mouseenter" ,function() {
+        $(".describe").hide();
+        $(".circuit").show();
+    });
+    $(".challengeB").on("mouseenter" ,function() {
+        $(".describe").hide();
+        $(".forest").show();
+    });
+    $(".qnaB").on("mouseenter" ,function() {
+        $(".describe").hide();
+        $(".house").show();
+    });
+    $(".refB").on("mouseenter" ,function() {
+        $(".describe").hide();
+        $(".mine").show();
+    });
+    
+    $(".highway").on("click", function() {//linking click to move page
+        location.href= "./highwayInstruction./HWI_index.html";
+    })
+})
