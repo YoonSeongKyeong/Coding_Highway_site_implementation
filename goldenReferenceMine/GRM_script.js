7db18ebcb6b23f7644ecb45c7989819bfb213518
mdy6099 <mdy6099@naver.com>
2019-06-27 17:26:44 +0900

    made  category page of each section

---
+++ goldenReferenceMine/GRM_script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".book").on("click", function() {//linking click to move page
+        location.href= " ";
+    })
+    $(".website").on("click", function() {//linking click to move page
+        location.href= " ";
+    })
+})
--- script.js
+++ script.js
@@ -3,28 +3,43 @@ $(document).ready(function() {
         $(".describe").hide();
         $(".highway").show();
     });
-    $(".defB").on("mouseenter" ,function() {
+    $(".defB").on("mouseenter" ,function() {//making photo show
         $(".describe").hide();
         $(".sunclear").show();
     });
-    $(".playB").on("mouseenter" ,function() {
+    $(".playB").on("mouseenter" ,function() {//making photo show
         $(".describe").hide();
         $(".circuit").show();
     });
-    $(".challengeB").on("mouseenter" ,function() {
+    $(".challengeB").on("mouseenter" ,function() {//making photo show
         $(".describe").hide();
         $(".forest").show();
     });
-    $(".qnaB").on("mouseenter" ,function() {
+    $(".qnaB").on("mouseenter" ,function() {//making photo show
         $(".describe").hide();
         $(".house").show();
     });
-    $(".refB").on("mouseenter" ,function() {
+    $(".refB").on("mouseenter" ,function() {//making photo show
         $(".describe").hide();
         $(".mine").show();
     });
     
     $(".highway").on("click", function() {//linking click to move page
         location.href= "./highwayInstruction/HWI_index.html";
+    })
+    $(".defB, .sunclear ").on("click", function() {//linking click to move page
+        location.href= "./sunclearDefVillage/SDV_index.html";
+    })
+    $(".playB, .circuit ").on("click", function() {//linking click to move page
+        location.href= "./hardGroundPlayCircuit/HGPC_index.html";
+    })
+    $(".challengeB, .forest ").on("click", function() {//linking click to move page
+        location.href= "./buggyForestChallenge/BFC_index.html";
+    })
+    $(".qnaB, .house ").on("click", function() {//linking click to move page
+        location.href= "./wonderfulQnAHouse/WQH_index.html";
+    })
+    $(".refB, .mine ").on("click", function() {//linking click to move page
+        location.href= "./goldenReferenceMine/GRM_index.html";
     })
 })
